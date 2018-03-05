026e3ea8d100a982ea16d43b9c9444dab2ad2023
Marioliniya <marioliniya@gmail.com>
2018-03-05 15:04:12 +0200

    ContentManager created

---
+++ GameWorlds/Model/Managers/ContentManager.h
@@ -0,0 +1,13 @@
+//
+//  ContentManager.h
+//  GameWorlds
+//
+//  Created by Admin on 3/5/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContentManager : NSObject
+
+@end
---
+++ GameWorlds/Model/Managers/ContentManager.m
@@ -0,0 +1,13 @@
+//
+//  ContentManager.m
+//  GameWorlds
+//
+//  Created by Admin on 3/5/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import "ContentManager.h"
+
+@implementation ContentManager
+
+@end
